dab378a81a60613fdfdade2a09bb06ac228a4e2f
SemihYlmzz <ekkotaymmm@gmail.com>
2023-03-02 16:33:46 +0300

    # main -> main + app

---
+++ lib/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:productivity/providers/today_provider.dart';
+import 'package:productivity/screens/productivity_screens.dart';
+import 'package:provider/provider.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => TodayProvider())],
+      child: CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        theme: CupertinoThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Colors.black87,
+          textTheme: CupertinoTextThemeData(
+            textStyle: GoogleFonts.dongle(
+              color: Colors.white,
+              fontSize: 35,
+              shadows: [
+                const Shadow(
+                  color: Colors.white,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+          ),
+        ),
+        title: 'Productivity',
+        home: const ProductivityScreens(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:productivity/providers/today_provider.dart';
-import 'package:productivity/screens/productivity_screens.dart';
-import 'package:provider/provider.dart';
+
+import 'app.dart';
 
 void main() async {
   runApp(
-    const MyApp(),
+    const App(),
   );
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => TodayProvider())],
-      child: CupertinoApp(
-        debugShowCheckedModeBanner: false,
-        theme: CupertinoThemeData(
-          brightness: Brightness.dark,
-          scaffoldBackgroundColor: Colors.black87,
-          textTheme: CupertinoTextThemeData(
-            textStyle: GoogleFonts.dongle(
-              color: Colors.white,
-              fontSize: 35,
-              shadows: [
-                const Shadow(
-                  color: Colors.white,
-                  blurRadius: 15,
-                ),
-              ],
-            ),
-          ),
-        ),
-        title: 'Productivity',
-        home: const ProductivityScreens(),
-      ),
-    );
-  }
 }
